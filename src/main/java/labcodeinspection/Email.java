eadc12510c08171e56b0fb249d2fee1b1ed28189
odarwin <43821334+odarwin@users.noreply.github.com>
2021-06-10 17:08:11 -0500

    without errors

--- src/main/java/labcodeinspection/Email.java
+++ src/main/java/labcodeinspection/Email.java
@@ -1,51 +1,94 @@
 package labcodeinspection;
-
+/**
+*
+*
+* @author Oscar Guaman
+*/
 public class Email {
+	/**
+	*
+	*
+	* variable M_FIRSTNAME
+	*/
+	private transient final String M_FIRSTNAME;
+	/**
+	* variable M_LASTNAME
+	*/
+	
+	private transient final String M_LASTNAME;
+	/**
+	* variable password
+	*/	
+	private static String password = "";
+	/**
+	* variable department
+	*/
+	private static String department = "";
+	/**
+	* variable PASSLEN
+	*/
+	private static final int PASSLEN = 8;
+	/**
+	* variable password
+	*/
+	private static String emainame;
 
-	private String m_firstName;
-	private String m_lastName;
-	private String password = null;
-	private String department;
-	private int defaultpasswordLength = 8;
-	private String email;
-
-	public Email(String firstName, String lastName) {
-		this.m_firstName = firstName;
-		this.m_lastName = lastName;
+	/**
+	* variable
+	*/
+	public Email(final String firstName, final String lastName) {
+		this.M_FIRSTNAME = firstName;
+		this.M_LASTNAME = lastName;
 	}
 
+	/**
+	* metodo showInfo Oscar
+	*/
 	public void showInfo() {
-		System.out.println("\nFIRST NAME= " + m_firstName + "\nLAST NAME= " + m_lastName);
-		System.out.println("DEPARMENT= " + department + "\nEMAIL= " + email + "\nPASSWORD= " + password);
+		System.out.println("\nFIRST NAME= " + M_LASTNAME + "\nLAST NAME= " + M_LASTNAME); 		
+		System.out.println("DEPARMENT= " + department + "\nEMAIL= " + emainame + "\nPASSWORD= " + password); 		
 	}
+	
+	/**
+	* setDeparment Oscar
+	*/
+	public void setDeparment(final int depChoice) {
+		switch (depChoice) { 		
 
-	public void setDeparment(int depChoice) {
-		switch (depChoice) {
-		case 1:
-			this.department = "sales";
+		case 1: 
+			Email.department = "sales";
 			break;
 		case 2:
-			this.department = "dev";
+			Email.department = "dev";
 			break;
 		case 3:
-			this.department = "acct";
+			Email.department = "acct";
+			break;
+		default:
+			Email.department="vacio";
 			break;
 		}
+		
 	}
-
-	private String randomPassword(int length) {
-		String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
-		char[] password = new char[length];
+	/**
+	* randomPassword Oscar
+	*/
+	private String randomPassword(final int length) {
+		final String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*"; //NOPMD
+		char[] password = new char[length]; //NOPMD
 		for (int i = 0; i < length; i++) {
-			int rand = (int) (Math.random() * set.length());
-			password[i] = set.charAt(rand);
+			final int rand = (int) (Math.random() * set.length()); 
+			password[i] = set.charAt(rand);//NOPMD
 		}
 		return new String(password);
 	}
-
+	
+	/**
+	* generateEmail Oscar
+	*/
 	public void generateEmail() {
-		this.password = this.randomPassword(this.defaultpasswordLength);
-		this.email = this.m_firstName.toLowerCase() + this.m_lastName.toLowerCase() + "@" + this.department
+		Email.password = this.randomPassword(Email.PASSLEN);
+		Email.emainame = this.M_FIRSTNAME.toLowerCase() + this.M_LASTNAME.toLowerCase() + "@" + Email.department 
 				+ ".espol.edu.ec";
 	}
 }
--- src/main/java/labcodeinspection/EmailApp.java
+++ src/main/java/labcodeinspection/EmailApp.java
@@ -1,26 +1,60 @@
 package labcodeinspection;
 
+
+
 import java.util.Scanner;
 
-public class EmailApp {
+/**
+* commentarios
+*/
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+public final class EmailApp {
 
-		System.out.print("Enter your first name: ");
-		String firstName = sc.nextLine();
-
-		System.out.print("Enter your last name: ");
-		String lastName = sc.nextLine();
-
-		System.out.print("\nDEPARTMENT CODE\n1. for sales\n2. for Development\n3. for accounting\nEnter code: ");
-
-		int depChoice = sc.nextInt();
-		sc.close();
-
-		Email email = new Email(firstName, lastName);
-		email.setDeparment(depChoice);
-		email.generateEmail();
-		email.showInfo();
+	private EmailApp() {
+	throw new UnsupportedOperationException();
 	}
+
+
+	/**
+	* comentario Darwin
+	*/
+	public static void main(final String args[] ) {
+
+		final Scanner scLong = new Scanner(System.in);
+		
+		try {
+			System.out.print("Enter your first name: ");
+			final String firstName = scLong.nextLine();
+			
+			
+			
+			System.out.print("Enter your last name: ");//NOPMD
+			final String lastName = scLong.nextLine();
+			
+			
+			
+			System.out.print("\nDEPARTMENT CODE\n1. for sales\n2. for Development\n3. for accounting\nEnter code: "); //NOPMD
+			
+			
+			
+			final int depChoice = scLong.nextInt();
+			
+			
+			
+			
+			final Email email = new Email(firstName, lastName);
+			email.setDeparment(depChoice);
+			email.generateEmail();
+			email.showInfo();
+		
+		}
+		finally {
+		
+			scLong.close();
+		}
+		
+	
+	
+	}
+
 }
